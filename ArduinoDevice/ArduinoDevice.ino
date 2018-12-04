dd02d5229595a309f14e0f6e93bc442b8d10626d
Joseph <jburley@spsu.edu>
2018-12-03 22:12:55 -0500

    Fixed Crashing Program now acts bizzare. Repeats setup portions of tasks when serial input is given.

--- ArduinoDevice/ArduinoDevice.ino
+++ ArduinoDevice/ArduinoDevice.ino
@@ -19,21 +19,29 @@ QueueHandle_t xHardwareQueue; //For transmitting data to the hardware task
    (1 byte opcode, 1 byte message length, 30 bytes message)
 */
 const int queueLength = 5;
-const int queueWidth = 32;
+
+//message struct for passing messages between tasks
+typedef struct
+{
+  int source;
+  int opcode;
+  int data;
+} message;
 
 void setup() {
   Serial.begin(9600);
+  Serial.setTimeout(45); //timeout is 45 ms (3 ticks)
 
   //first queues are made
-  xSerialQueue = xQueueCreate(queueLength, queueWidth);
+  xSerialQueue = xQueueCreate(queueLength, sizeof(message));
   //xEthernetQueue = xQueueCreate(queueLength, queueWidth);
-  xHardwareQueue = xQueueCreate(queueLength, queueWidth);
+  xHardwareQueue = xQueueCreate(queueLength, sizeof(message));
 
-  
 
-  
 
-  xTaskCreate(TaskSerial, 
+
+
+  xTaskCreate(TaskSerial,
               (const portCHAR*) "Serial",
               128,
               NULL,
@@ -47,7 +55,7 @@ void setup() {
               1,
               &hardwareHandle);
 
-  
+
 }
 
 void loop() {
@@ -58,57 +66,98 @@ void loop() {
 
 void TaskSerial( void* pvParameters)
 {
-  Serial.write((int) xSerialQueue );
+  Serial.print((int) xSerialQueue, HEX);
   Serial.write('\n');
-  Serial.write((int) xHardwareQueue);
+  Serial.print((int) xHardwareQueue, HEX);
   Serial.write('\n');
-  Serial.write((int) serialHandle);
+  Serial.print((int) serialHandle, HEX);
   Serial.write('\n');
-  Serial.write((int) hardwareHandle);
+  Serial.print((int) hardwareHandle, HEX);
   Serial.write('\n');
+
+  int max_stack = 0;
 
   /*
      This task listens on the serial port and serial queue.
      When a message arrives it validates it before sending it along.
   */
-  char* messageBuffer [32];
+  message SerialMessage;
+  SerialMessage.source = 0;
+  SerialMessage.opcode = 0;
+  SerialMessage.data = 0;
+
+  String tempMess;
+  tempMess.reserve(20);
 
   for (;;)
   {
-    
+
     if (Serial.available() > 0)
     {
-      Serial.println('I');
-      if (Serial.available() > 32)
+      while(Serial.available())
       {
-        Serial.println("Input too long");
+        tempMess += Serial.read();
+      }
+      tempMess.trim();
+      int firstSpace = tempMess.indexOf(' '); //if this is -1 the string has only one argument and is invalid
+      if (firstSpace == -1)
+      {
+        Serial.println(F("Invalid Command: Too few arguments"));
+        break;
+      }
+      int lastSpace = tempMess.indexOf(' ', firstSpace); //if this is -1 the string has only two arguments and must be a read op
+      String paramNumArg = tempMess.substring(firstSpace, lastSpace); //0-127 this is the number of the input or output
+      String paramDatArg = tempMess.substring(lastSpace); //if output, then is unused. if input, is the value to write
+
+      int paramNum = paramNumArg.toInt();
+      int paramDat = paramDatArg.toInt();
+
+      if (tempMess.charAt(0) == 'R') //user is reading from output
+      {
+        SerialMessage.opcode |= (1 << 7); //set bit 7
+      }
+      else if (tempMess.charAt(0) == 'W') //user is writing to input
+      {
+        SerialMessage.opcode &= !(1 << 7); //reset bit 7
       }
       else
       {
-        Serial.println("here");
-        //read message into buffer
-        Serial.readBytes(*messageBuffer, 32); //<--- The source of the bug is probably here. readBytes may be blocking execution
+        Serial.println(F("Invalid Command: Must be R/W"));
+        break;
+      }
 
-        /*
-           There is no other input validation at this time.
-           Simply enqueue the message and clear the buffer
-        */
-        Serial.println("free space: " + uxQueueSpacesAvailable(xHardwareQueue));
-        bool succ = xQueueSend(xHardwareQueue, messageBuffer, 0);
-        if(!succ)
-        {
-          Serial.println("queue full");
-        }
-        *messageBuffer = 0;
+      SerialMessage.opcode |= paramNum;
+      SerialMessage.data = paramDat;
+
+
+      /*
+        There is no other input validation at this time.
+        Simply enqueue the message and clear the buffer
+      */
+      Serial.print(uxQueueSpacesAvailable(xHardwareQueue), DEC);
+      bool succ = xQueueSend(xHardwareQueue, &SerialMessage, 0);
+      if (!succ)
+      {
+        Serial.println("queue full");
       }
     }
 
-    if (xQueueReceive(xSerialQueue, messageBuffer, 1) == pdPASS)
+
+    if (xQueueReceive(xSerialQueue,&SerialMessage, 1) == pdPASS)
     {
       Serial.println("Received message");
-      Serial.write((char*) messageBuffer, 32);
-      *messageBuffer = 0;
+      Serial.println(SerialMessage.source);
+      Serial.println(SerialMessage.opcode);
+      Serial.println(SerialMessage.data);
     }
+
+    int temp = uxTaskGetStackHighWaterMark(NULL);
+    if (temp > max_stack)
+    {
+      max_stack = temp;
+      Serial.write(max_stack);
+    }
+
   }
 
 }
@@ -125,16 +174,19 @@ void TaskHardware(void* pvParameters)
   //I don't think the queue handles need to be passed into the parameters
 
   //define output pin numbers
-  const int num_out = 3;
-  const int out_pins[] = {7, 6, 5};
+  const unsigned int num_out = 3;
+  const unsigned int out_pins[] = {7, 6, 5};
   bool out_val_curr[num_out];
   bool out_val_next[num_out];
 
   //a char buffer to store data
-  char* messBuff [32];
+  message HardwareMessage;
+  HardwareMessage.source = 0;
+  HardwareMessage.opcode = 0;
+  HardwareMessage.data = 0;
 
   //set pins to output mode and set low
-  for(int i=0; i<num_out; i++)
+  for (int i = 0; i < num_out; i++)
   {
     out_val_curr[i] = HIGH;
     out_val_next[i] = HIGH;
@@ -148,82 +200,61 @@ void TaskHardware(void* pvParameters)
   {
     //for now only reads from queue
     //by default 1 tick is 15ms
-    if (xQueueReceive(xHardwareQueue, messBuff, 0) != pdPASS)
+    if (xQueueReceive(xHardwareQueue, &HardwareMessage, 1) != pdPASS)
     {
       //normally here is where I would check for button inputs
-      if(digitalRead(8))
+      if (digitalRead(8))
       {
-        out_val_next[0] = !out_val_next[0];
+        out_val_next[2] = !out_val_next[2];
       }
     }
-    else
+    else //if  message on hardware queue
     {
       /*
          Check bit 5 of first byte.
          If 1, is applying input
          If 0, is reading current output
       */
-      
-      //the number of the input or output being referenced
-      byte num = *messBuff[0] & 0x1F;
+      Serial.print("message received");
 
-      if (*messBuff[0] & (1 << 5))
+      //the number of the input or output being referenced
+      unsigned int rw = HardwareMessage.opcode & (1 << 7); //MSB of opcode is for r/w select
+      unsigned int num = HardwareMessage.opcode & !(1 << 7); //grab the 7 LSB of opcode
+
+      if(rw == 1)
       {
-        /* Input
-           mask 5 LSB to determine input number to change
-           if greater than total number of inputs reject it
-        */
-        //build the value being input
-        unsigned long val = 0;
-        for (byte i = *messBuff[1]; i > 0; i--)
-        {
-          val += *messBuff[i + 2] << (i * 8);
-        }
-        bool val_bool = (bool) val;
-        if (num < num_out)
-        {
-          out_val_next[num] = val_bool;
-        }
+        //read output
+        HardwareMessage.data = out_val_curr[num];
+        xQueueSend(xSerialQueue, &HardwareMessage, 0);
+      }
+      else if(rw == 0)
+      {
+        //write input
+        out_val_next[num] = HardwareMessage.data;
       }
       else
       {
-        /*
-         * Output
-         * A read request will never be more than 1 byte long
-         */
-
-        *messBuff[1] = 1;
-        *messBuff[2] = out_val_curr[num];
-        xQueueSend(xSerialQueue, messBuff, 1);
+        Serial.println("wat");
       }
 
-      
+
     }
 
     /*
-     * Check out_val_curr against our_val_prev for differences
-     * If there are differences, apply changes to pins
-     * And write new values from curr to prev
-     */
+       Check out_val_curr against our_val_prev for differences
+       If there are differences, apply changes to pins
+       And write new values from curr to prev
+    */
 
-    bool sent = false;
-    for(int i=0; i<num_out; i++)
+    for (int i = 0; i < num_out; i++)
     {
-      if(out_val_curr[i] != out_val_next[i])
+      if (out_val_curr[i] != out_val_next[i])
       {
-        sent = true;
         digitalWrite(out_pins[i], out_val_next[i]);
         out_val_curr[i] = out_val_next[i];
-        
+
       }
     }
-
-    if(sent)
-    {
-     *messBuff[0] = 'f'; 
-    }
-
-    *messBuff = 0;
     vTaskDelay( 1 );
   }
 }
